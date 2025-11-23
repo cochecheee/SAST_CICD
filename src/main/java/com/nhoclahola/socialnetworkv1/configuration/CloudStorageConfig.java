df8cf29c6ccafd40d82591fab724fe865e02aeef
cochecheee <buitien747@gmail.com>
2025-11-24 01:30:11 +0700

    ..cd

--- src/main/java/com/nhoclahola/socialnetworkv1/configuration/CloudStorageConfig.java
+++ src/main/java/com/nhoclahola/socialnetworkv1/configuration/CloudStorageConfig.java
@@ -22,6 +22,11 @@ public class CloudStorageConfig {
     private String awsRegion;
 
     @Bean
+    @ConditionalOnProperty(
+            name = "aws.s3.enabled",
+            havingValue = "true",
+            matchIfMissing = false
+    )
     public S3Client generateS3Client() {
         if (awsAccessKeyId == null || awsAccessKeyId.isBlank() ||
                 awsSecretAccessKey == null || awsSecretAccessKey.isBlank()) {
